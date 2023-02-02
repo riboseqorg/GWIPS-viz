4eedb36bafd72bdcfb88b85214fbfbc6a4a1cb64
codemeleon <anmol.kiran@gmail.com>
2023-02-02 22:18:28 +0000

    gwips html+css

---
+++ htdocs/js/hgCustom.js
@@ -0,0 +1,12 @@
+function submitClick(ele)
+{
+// Tell the user we are processing the upload when the user clicks on the submit button.
+    loadingImage.run();
+//    alert("Unfortunately Custom Tracks are currently unavailable. Please check back soon")
+    return true;
+}
+
+$(document).ready(function()
+{
+    loadingImage.init($("#loadingImg"), $("#loadingMsg"), "<p style='color: red; font-style: italic;'>Uploading and processing your data may take some time. Please leave this window open while your custom track is loading.</p>");
+});
